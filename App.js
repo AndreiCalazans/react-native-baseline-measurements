05147a119d3c41f73fe16e405676eb2d3e04e28c
Andrei Calazans <andrei.calazans@coinbase.com>
2022-06-28 08:38:13 -0300

    Install rn-peformance lib

--- App.js
+++ App.js
@@ -6,13 +6,46 @@
  * @flow strict-local
  */
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
+import performance, {PerformanceObserver} from 'react-native-performance';
+
+performance.mark('app_file');
+
+const measureObserver = new PerformanceObserver((list, _) => {
+  console.log('**measure**');
+  list.getEntries().forEach(entry => {
+    console.log(`marker: ${entry.name} took ${entry.duration}ms`);
+  });
+});
+
+measureObserver.observe({type: 'measure', buffered: true});
+
+new PerformanceObserver((list, _) => {
+  console.log('**native**');
+  list.getEntries().forEach(entry => {
+    console.log(`marker: ${entry.name} took ${entry.duration}ms`);
+  });
+
+  if (list.getEntries().find(entry => entry.name === 'runJsBundleEnd')) {
+    // nativeLuanch - how long it takes to initialize the native context
+    // this includes native module initialization + the full React context.
+    performance.measure('nativeLaunch', 'nativeLaunchStart', 'nativeLaunchEnd');
+
+    // runJsBundle - how long it takes to parse the JS bundle.
+    performance.measure('runJsBundle', 'runJsBundleStart', 'runJsBundleEnd');
+  }
+}).observe({type: 'react-native-mark', buffered: true});
+
 function HomeScreen() {
+  useEffect(() => {
+    performance.measure('home', 'app_file');
+  }, []);
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
