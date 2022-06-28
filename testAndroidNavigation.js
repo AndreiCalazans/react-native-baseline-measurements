c468fc1f3848a7e8ec9fc0b506ce68dc5f87380e
Andrei Calazans <andrei.calazans@coinbase.com>
2022-06-28 13:10:30 -0300

    Format testAndroidNavigation

--- testAndroidNavigation.js
+++ testAndroidNavigation.js
@@ -11,13 +11,13 @@ const _exec = async command => {
   });
 };
 
-const sleep = (ms) => new Promise(res => setTimeout(res, ms));
+const sleep = ms => new Promise(res => setTimeout(res, ms));
 
 const main = async () => {
   const clear = 'adb logcat -c';
   const stop = 'adb shell am force-stop com.testreactnative';
   const open = 'adb shell monkey -p com.testreactnative 1';
-	const press = 'adb shell input tap 739 1527';
+  const press = 'adb shell input tap 739 1527';
 
   console.log('clearing logs');
   await _exec(clear);
@@ -25,9 +25,9 @@ const main = async () => {
   for (let i = 0; i < 9; i++) {
     console.log('run - ', i);
     await _exec(open);
-		await sleep(1500);
-		await _exec(press);
-		await sleep(1000);
+    await sleep(1500);
+    await _exec(press);
+    await sleep(1000);
     await _exec(stop);
   }
   console.log('retrieving logs');
